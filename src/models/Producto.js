345896b33fbfb27058cf5e1d468fde05bba3a3bb
davidcatalan9 <dcatalan@gmail.com>
2024-11-12 21:32:56 -0300

    feat: usuario y producto1

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -14,6 +14,48 @@ class Product {
         this.#description = description;
         this.#price = price;
         this.#stock = stock;
-
     }
+
+    get id() {
+        return this.#id;
+    }
+    get name() { 
+        return this.#name;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get price() { 
+        return this.#price;
+    }
+    get stock() { 
+        return this.#stock;
+    }
+
+    setName(newName) {
+        this.#name = newName
+    }
+    setDescription(newDescription) {
+        this.#description = newDescription
+    }
+    setPrice(newPrice) {
+        this.#price = newPrice
+    }
+    setStock(newStok) { 
+        this.#stock = newStock
+    }
+
+    getAllProperties() {
+        return {
+            id: this.#id,
+            name: this.#name,
+            description: this.#description,
+            price: this.#price,
+            stock: this.#stock
+            active: this.#visible
+        }
+    }
+
 }
